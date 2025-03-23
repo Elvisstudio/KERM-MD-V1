0042b4b15c87c5c6934deb6520494f0c561efdc6
Elvisstudio <papapopoola619@gmail.com>
2025-03-23 13:01:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",KERM-MD-V1~uNQhxZSQ#junmekFXvstldpiJIpI5oIy2mTvXpXSGb7cXF2zKMyM
+SESSION_ID: process.env.SESSION_ID || "KERM-MD-V1~uNQhxZSQ#junmekFXvstldpiJIpI5oIy2mTvXpXSGb7cXF2zKMyM,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
